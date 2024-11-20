f72d525e8492c59c89ef811c769cb5634ba4211a
Andre Ferreira <andre.ferreira@ubisoft.com>
2024-11-19 23:29:24 -0500

    Add spring skeleton

---
+++ src/test/java/com/andreferreira/tinybank/TinyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreferreira.tinybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
